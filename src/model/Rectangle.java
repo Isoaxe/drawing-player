21fd00b91cba88cfedd0640e6399442f2f307108
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-08-08 20:41:57 +0100

    Rectangle: Add rectangle-specific methods
    These are taken directly from Shape.

--- src/model/Rectangle.java
+++ src/model/Rectangle.java
@@ -8,4 +8,22 @@ public class Rectangle extends Shape {
     public Rectangle(Point topLeft, MidiSynth midiSynth) {
         super(topLeft, midiSynth);
     }
+
+    // EFFECTS: return true iff the given x value is within the bounds of the Shape
+    public boolean containsX(int x){
+        return (this.x <= x) && (x <= this.x + width);
+    }
+
+    // EFFECTS: return true iff the given y value is within the bounds of the Shape
+    public boolean containsY(int y) {
+        return (this.y <= y) && (y <= this.y + height);
+    }
+
+    // EFFECTS: return true if the given Point (x,y) is contained within the bounds of this Shape
+    public boolean contains(Point point) {
+        int point_x = point.x;
+        int point_y = point.y;
+
+        return containsX(point_x) && containsY(point_y);
+    }
 }
