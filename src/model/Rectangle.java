67938796f17bb9c9583739ee84a2bffe9db014eb
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-08-10 11:46:51 +0100

    Rectangle: Add drawing methods here
    Move the drawGraphics and fillGraphics methods from Shape to Rectangle. These implementations are specific to drawing a rectangle and not other shapes.

--- src/model/Rectangle.java
+++ src/model/Rectangle.java
@@ -19,4 +19,16 @@ public class Rectangle extends Shape {
     public boolean contains(Point point) {
         return containsX(point.x) && containsY(point.y);
     }
+
+    //EFFECTS: draws the shape
+    @Override
+    protected void drawGraphics(Graphics g) {
+        g.drawRect(x, y, width, height);
+    }
+
+    //EFFECTS: fills the shape
+    @Override
+    protected void fillGraphics(Graphics g) {
+        g.fillRect(x, y, width, height);
+    }
 }
