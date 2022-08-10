37ab8fa2c5eaacd68dc0bf9a4e32c8903a81dfc8
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-08-10 13:02:35 +0100

    Oval: Assign colour in constructor
    Assign the PLAYING_COLOR variable in the Oval constructor instead of in its superclass.

--- src/model/Oval.java
+++ src/model/Oval.java
@@ -7,6 +7,7 @@ import java.awt.*;
 public class Oval extends Shape {
     public Oval(Point topLeft, MidiSynth midiSynth) {
         super(topLeft, midiSynth);
+        PLAYING_COLOR = new Color(0, 128, 128);
     }
 
     // EFFECTS: return true if this Oval contains the given point p, else return false
