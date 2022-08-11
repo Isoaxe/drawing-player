1b8eeae5bcc5d88c99d8f791e6fef0d831e6aeb8
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-08-11 18:32:38 +0100

    Create RectangleTool.java
    Add a RectangleTool that is a subclass of ShapeTool and implement a constructor that matches its superclass.

---
+++ src/ui/tools/RectangleTool.java
@@ -0,0 +1,11 @@
+package ui.tools;
+
+import ui.DrawingEditor;
+
+import javax.swing.*;
+
+public class RectangleTool extends ShapeTool {
+    public RectangleTool(DrawingEditor editor, JComponent parent) {
+        super(editor, parent);
+    }
+}
