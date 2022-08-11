f8c66ace44b2f5e7cc30d647fff469b250a17e47
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-08-11 18:37:58 +0100

    RectangleTool: Add makeShape method
    Method was moved from ShapeTool but instantiated as a Rectangle rather than Oval.

--- src/ui/tools/RectangleTool.java
+++ src/ui/tools/RectangleTool.java
@@ -1,11 +1,18 @@
 package ui.tools;
 
+import model.Rectangle;
 import ui.DrawingEditor;
 
 import javax.swing.*;
+import java.awt.event.MouseEvent;
 
 public class RectangleTool extends ShapeTool {
     public RectangleTool(DrawingEditor editor, JComponent parent) {
         super(editor, parent);
+    }
+
+    // EFFECTS: Constructs and returns the new rectangle
+    private void makeShape(MouseEvent e) {
+        shape = new Rectangle(e.getPoint(), editor.getMidiSynth());
     }
 }
