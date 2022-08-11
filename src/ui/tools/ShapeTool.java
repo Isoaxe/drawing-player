737bafdf72ba6b93e611054a5a8759b7c2229e02
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-08-11 19:32:31 +0100

    ShapeTool: Make class abstract
    Add the abstract keyword in the class declaration.

--- src/ui/tools/ShapeTool.java
+++ src/ui/tools/ShapeTool.java
@@ -8,7 +8,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 
-public class ShapeTool extends Tool {
+public abstract class ShapeTool extends Tool {
 	protected Shape shape;
 
     public ShapeTool(DrawingEditor editor, JComponent parent) {
