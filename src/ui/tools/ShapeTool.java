bf167f8c227dc7089a411b1e8f96471b816a4147
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-08-12 08:23:21 +0100

    ShapeTool: Make class concrete
    Remove the abstract keyword from the class declaration. This class will now be instantiable.

--- src/ui/tools/ShapeTool.java
+++ src/ui/tools/ShapeTool.java
@@ -9,7 +9,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 
-public abstract class ShapeTool extends Tool {
+public class ShapeTool extends Tool {
 	protected Shape shape;
     private String label;
 
